e55466c1d3bb1060b4a336e0e94fdc91c92b8a08
Mikegdro <miguelgdro@gmail.com>
2023-06-18 21:45:46 +0200

    Iconos default achievements

--- migrations/2023-04-24-163742_create_achievement/up.sql
+++ migrations/2023-04-24-163742_create_achievement/up.sql
@@ -9,15 +9,15 @@ CREATE TABLE achievement (
 );
 
 INSERT INTO achievement VALUES
-('1', 'Befriend', 'Make friends on the platform', 'friends.png', 'friend', '{1, 5, 20}'), -- Implementado
-('2', 'Pioneer', 'Create your own projects', 'projects.png', 'project', '{1, 5, 25}'), -- Implementado
-('3', 'Kanbanize', 'Utilize Kanban App in projects', 'kanban.png', 'app', '{1, 5}'), -- Implementado
-('4', 'Timeliner', 'Utilize Timeline App in projects', 'timeline.png', 'app', '{1, 5}'), -- Implementado
-('5', 'Project Party', 'Invite anyone to join your projects', 'invite.png', 'project', '{1, 20, 50}'), -- Implementado
-('6', 'Column Creator', 'Create Kanban columns', 'columns.png', 'app', '{1, 15, 40}'), -- Implementado
-('7', 'App Installer', 'Install complementary apps for projects', 'install.png', 'app', '{1, 25, 100}'), -- Implementado
-('8', 'Kanban Commander', 'Assign tasks to project members in Kanban', 'assign.png', 'app', '{1, 5, 25}'), -- Implementado
-('9', 'Timeline Captain', 'Assign tasks to project members in timeline', 'assign.png', 'app', '{1, 5, 25}'), -- Implementado
-('10', 'Chat Starter', 'Initiate a chat conversation', 'chat.png', 'friend', '{1}'),
-('11', 'Git Integration', 'Link Git branch with project tasks', 'git.png', 'friend', '{1, 5}'),
-('12', 'Project Joiner', 'Join an existing project', 'join.png', 'project', '{1, 5, 20}') -- Implementado
+('1', 'Befriend', 'Make friends on the platform', 'https://www.svgrepo.com/show/513065/wifi-1012.svg', 'friend', '{1, 5, 20}'), -- Implementado
+('2', 'Pioneer', 'Create your own projects', 'https://www.svgrepo.com/show/512254/flag-bug-report-1493.svg', 'project', '{1, 5, 25}'), -- Implementado
+('3', 'Kanbanize', 'Utilize Kanban App in projects', 'https://www.svgrepo.com/show/512604/object-placement-65.svg', 'app', '{1, 5}'), -- Implementado
+('4', 'Timeliner', 'Utilize Timeline App in projects', 'https://www.svgrepo.com/show/512922/stats-1369.svg', 'app', '{1, 5}'), -- Implementado
+('5', 'Project Party', 'Invite anyone to join your projects', 'https://www.svgrepo.com/show/512401/key-677.svg', 'project', '{1, 20, 50}'), -- Implementado
+('6', 'Column Creator', 'Create Kanban columns', 'https://www.svgrepo.com/show/511552/bookmark-plus-fill-1237.svg', 'app', '{1, 15, 40}'), -- Implementado
+('7', 'App Installer', 'Install complementary apps for projects', 'https://www.svgrepo.com/show/512290/folder-plus-1782.svg', 'app', '{1, 25, 100}'), -- Implementado
+('8', 'Kanban Commander', 'Assign tasks to project members in Kanban', 'https://www.svgrepo.com/show/512991/tie-763.svg', 'app', '{1, 5, 25}'), -- Implementado
+('9', 'Timeline Captain', 'Assign tasks to project members in timeline', 'https://www.svgrepo.com/show/512992/tie-765.svg', 'app', '{1, 5, 25}'), -- Implementado
+('10', 'Chat Starter', 'Initiate a chat conversation', 'https://www.svgrepo.com/show/512468/message-1579.svg', 'friend', '{1}'),
+('11', 'Git Integration', 'Link Git branch with project tasks', 'https://www.svgrepo.com/show/512317/github-142.svg', 'friend', '{1, 5}'),
+('12', 'Project Joiner', 'Join an existing project', 'https://www.svgrepo.com/show/511307/airplane-mode-1406.svg', 'project', '{1, 5, 20}') -- Implementado
